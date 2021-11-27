4eade5f07001bd64d8e1d03f416e0a4c7601a04f
Danila Arkhipov <dan.arxipow2011@ya.ru>
2021-11-27 11:47:35 +0300

    immit

--- Course/Module 20/Task 1/Task1.cpp
+++ Course/Module 20/Task 1/Task1.cpp
@@ -9,14 +9,26 @@ int main()
         std::string date_salary;
         int salary;
 
-        std::cout << "Input name:" << std::endl;
-        std::cin >> name;
-        std::cout << "Input surname:" << std::endl;
-        std::cin >> surname;
-        std::cout << "Input date of salary:" << std::endl;
-        std::cin >> date_salary;
-        std::cout << "Input size of salary in RUB:" << std::endl;
-        std::cin >> salary;
+        while(1)
+        {
+                std::cout << "Input name:" << std::endl;
+                std::cin >> name;
+                std::cout << "Input surname:" << std::endl;
+                std::cin >> surname;
+                std::cout << "Input date of salary:" << std::endl;
+                std::cin >> date_salary;
+                std::cout << "Input size of salary in RUB:" << std::endl;
+                std::cin >> salary;
+                int x = std::stoi(date_salary.substr(0, 2));
+                x = std::stoi(date_salary.substr(3, 2));
+                x = std::stoi(date_salary.substr(6, 4));
+                if (std::stoi(date_salary.substr(0, 2)) < 1 or std::stoi(date_salary.substr(0, 2)) > 31 
+                        or std::stoi(date_salary.substr(3, 2)) < 0 or std::stoi(date_salary.substr(3, 2)) > 12
+                        or std::stoi(date_salary.substr(6, 4)) < 0 or std::stoi(date_salary.substr(6, 4)) > 2021)
+                                std::cout << "Wrong data. Try again" << std::endl;
+                else
+                        break;
+        }
 
         file << name << " ";
         file << surname << " ";
