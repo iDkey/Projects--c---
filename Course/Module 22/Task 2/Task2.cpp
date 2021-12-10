f238e701672931517202e01076164a0f110cf2fc
Danila Arkhipov <dan.arxipow2011@ya.ru>
2021-12-10 23:25:02 +0300

    immit

--- Course/Module 22/Task 2/Task2.cpp
+++ Course/Module 22/Task 2/Task2.cpp
@@ -4,5 +4,33 @@
 
 int main()
 {
-    
+    std::map<std::string, int> registry;
+    std::string data_in;
+    while(data_in != "exit")
+    {
+        std::cout << "Input a string:" << std::endl;
+        std::cin >> data_in;
+        if(data_in == "Next")
+        {
+            std::map<std::string, int>::iterator it = registry.begin();
+            std::cout << it -> first << std::endl;
+            it -> second -= 1;
+            if (it -> second == 0)
+            {
+                registry.erase(it);
+            }
+            
+        }
+        else
+        {
+            if(registry.count(data_in) > 0)
+            {
+                registry[data_in] += 1;
+            }
+            else
+            {
+                registry.insert(std::pair<std::string, int> (data_in, 1));
+            }
+        }
+    }
 }
--- test.cpp
+++ test.cpp
@@ -1,6 +1,56 @@
 #include <iostream>
- 
-int main()
-{
+#include <map>
+#include <string>
 
+using namespace std;
+
+bool requestIsSurname(const string&);
+
+int main() {
+
+  string request;
+  getline(cin, request);
+
+  map<string, string> oMap;
+  for(int i = 0; i < 10; ++i)
+    oMap.insert(pair<string, string> ("67-68-9" + to_string(i), "Ivanov"));
+
+  if(request.find(' ') != string::npos)  {
+    string number = request.substr(0,request.find(' '));
+    string surname = request.substr(request.find(' ') + 1, request.length());
+
+    oMap.insert(pair<string, string> (number, surname));
+    
+    system("clear");
+    for(auto& item : oMap)
+      cout << item.first << " " << item.second << endl;
+
+  } else if(requestIsSurname(request))  {
+    system("clear");
+    cout << "Surname: " << request << endl;
+    cout << "Phone number: ";
+
+    for(auto& item : oMap) {
+      if(item.second == request)
+      {
+        cout << " " << item.first ;
+      }
+    }
+    cout << endl;
+  } else  {
+    system("clear");
+    cout << "Surname: " << request;
+
+    cout << oMap[request] << endl;
+  }
+}
+
+bool requestIsSurname(const string& request){
+  for(int i = 0; i < request.length(); ++i)  {
+    if(!(request[i] >= 'a' && request[i] <= 'z') && !(request[i] >= 'A' && request[i] <= 'Z'))
+    {
+      return false;
+    }
+  }
+  return true;
 }
