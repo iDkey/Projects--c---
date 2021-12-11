3602a8914c671bfaef0e73d7d352bc65a7cb4006
Danila Arkhipov <dan.arxipow2011@ya.ru>
2021-12-11 16:50:06 +0300

    immit

--- Course/Module 12/12.4.2/RowOfNumbers.cpp
+++ Course/Module 12/12.4.2/RowOfNumbers.cpp
@@ -3,7 +3,7 @@
 int main()
 {
     int numbers[15] = {114,111, 106, 107, 108, 105, 115,  108, 110, 109, 112, 113, 116, 117, 118};
-    int min = MAX_INT;
+    int min = INT_MAX;
     int Sum1 = 0;
     for (int i = 0; i < 15; i++)
     {
