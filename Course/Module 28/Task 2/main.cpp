ee4f81ab0b44a58361dbe021e9361b391e45f3af
iDkey <dan.arxipow2011@ya.ru>
2022-02-03 11:25:00 +0300

    M28T2_fix

--- Course/Module 28/Task 2/main.cpp
+++ Course/Module 28/Task 2/main.cpp
@@ -1,4 +1,4 @@
-#include <iostream>
+/*#include <iostream>
 #include <thread>
 #include <mutex>
 #include <string>
@@ -14,7 +14,6 @@ void moveTrain(int timeTrain, char typeTrain)
     {
         if(acesToArrival.try_lock())
         {
-            //acesToArrival.try_lock();
             std::cout << "Train " << typeTrain << " has arrived" << std::endl;
             std::string departure;
             while(departure != "depart")
@@ -35,6 +34,58 @@ void moveTrain(int timeTrain, char typeTrain)
             }
         }
     }
+}
+
+int getTravelTime(int trainNumber)
+{
+    int timeTrain = 0;
+    std::cout << "Input time for travel for train " << trainNumber << ": " << std::endl;
+    std::cin >> timeTrain;
+    return timeTrain;
+}
+
+int main() {
+    int timeTrainA;
+    int timeTrainB;
+    int timeTrainC;
+    timeTrainA = getTravelTime(1);
+    timeTrainB = getTravelTime(2);
+    timeTrainC = getTravelTime(3);
+
+    std::thread travelTrain1(moveTrain, timeTrainA, 'A');
+    std::thread travelTrain2(moveTrain, timeTrainB, 'B');
+    std::thread travelTrain3(moveTrain, timeTrainC, 'C');
+    travelTrain1.join();
+    travelTrain2.join();
+    travelTrain3.join();
+    std::cout << "All trains has arrived" << std::endl;
+}*/
+
+#include <iostream>
+#include <thread>
+#include <mutex>
+#include <string>
+
+std::mutex arriveMtx;
+std::mutex waitingMtx;
+
+void moveTrain(int timeTrain, char typeTrain)
+{
+    std::cout << "Train " << typeTrain << " starts its travel" << std::endl;
+    std::this_thread::sleep_for(std::chrono::seconds(timeTrain));
+    std::cout << "Arrived train "<< typeTrain << std::endl;
+    arriveMtx.lock();
+    std::cout << "Waiting train "<< typeTrain << std::endl;
+    waitingMtx.lock();
+    std::string departure;
+    while(departure != "depart")
+    {
+        std::cout << "Input 'depart' when train release the station" << std::endl;
+        std::cin >> departure;
+    }
+    waitingMtx.unlock();
+    std::cout << "Train " << typeTrain << " departed" << std::endl;
+    arriveMtx.unlock();
 }
 
 int getTravelTime(int trainNumber)
