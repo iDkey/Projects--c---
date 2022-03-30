2f57a317d1c82a70d25be56d0f3dc2695ae11b03
iDkey <dan.arxipow2011@ya.ru>
2022-03-30 17:48:17 +0300

    commit

--- Course/Module 31/Task 1/main.cpp
+++ Course/Module 31/Task 1/main.cpp
@@ -16,7 +16,7 @@ class shared_ptr_toy
 {
 private:
 	Toy* obj;
-	int *count = new int(1);
+	int *count = new int(0);
 
 public:
 	shared_ptr_toy()
@@ -34,7 +34,6 @@ public:
 	{
 		std::cout << "copy obj" << std::endl;
 		*count+=1;
-		//*oth.count+=1;
 		obj = new Toy(*oth.obj);
 	}
 	shared_ptr_toy& operator = (const shared_ptr_toy& oth)
@@ -51,14 +50,19 @@ public:
 
 	~shared_ptr_toy()
 	{
-		*count -= 1;
-		delete obj;
-		std::cout << "del obj" << std::endl;
 		if(*count == 0)
 		{
 			delete count;
 			std::cout << "Del count" << std::endl;
 		}
+		else
+		{
+			*count -= 1;
+			delete obj;
+			std::cout << "del obj" << std::endl;
+		}
+
+
 	}
 };
 
@@ -85,9 +89,14 @@ public:
 
 };
 
+void make_shared(shared_ptr_toy& toy)
+{
+	std::unique_ptr<Dog> d1 = std::make_unique<Dog>("sharik", toy, 5);
+}
 
 int main() {
 	shared_ptr_toy ball("ball");
+	make_shared(ball);
 	std::unique_ptr<Dog> d1 = std::make_unique<Dog>("sharik", ball, 5);
 	std::unique_ptr<Dog> d2 = std::make_unique<Dog>("bobik", ball, 5);
 }
